ec155d9d58a10d2015bcc1cd8f373e496a7e2506
Yen Tran <yentran@Yens-MacBook-Pro.local>
2014-10-01 16:17:41 -0700

    User can create and delete a workout

--- app/controllers/workout.rb
+++ app/controllers/workout.rb
@@ -2,3 +2,23 @@ get '/workouts' do
 	@workouts = Workout.all 
 	erb :workouts
 end
+
+get '/workouts/new' do
+	erb :create_workout
+end
+
+get '/workouts/:id' do
+	@workout = Workout.find(params[:id])
+	erb :workouts_id
+end
+
+post '/workouts' do
+	@workout = Workout.create(name: params[:name])
+	redirect "/workouts/#{@workout.id}"
+end
+
+delete '/workouts/:id/delete' do
+	workout = Workout.find(params[:id])
+	workout.destroy
+	redirect '/workouts'
+end
