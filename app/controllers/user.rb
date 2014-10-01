582ed404f5bde815c874a10373bcf978028cfe2e
Yen Tran <yentran@Yens-MacBook-Pro.local>
2014-10-01 00:45:51 -0700

    User can now sign-up and sign-in (authentication)

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -6,6 +6,7 @@ post '/users' do
 	user = User.new
 	user.user_name = params[:user_name]
 	user.password_hash = BCrypt::Password.create(params[:password])
-	user.created_at = Time.now
-	user.update_at = Time.now
+	user.save
+
+	redirect '/sessions/new'
 end
